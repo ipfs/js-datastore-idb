2a71763ce41f76d7e8edc8a421741ee2b9c33455
Hugo Dias <hugomrdias@gmail.com>
2020-06-16 13:06:38 +0100

    fix: add concurrency test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 'use strict'
 
+const { expect } = require('aegir/utils/chai')
 const { MountDatastore } = require('datastore-core')
 const { Key } = require('interface-datastore')
 const { isNode } = require('ipfs-utils/src/env')
@@ -50,6 +51,73 @@ describe('IndexedDB Datastore', function () {
       teardown () {
         return Promise.all([one.destroy(), two.destroy(), three.destroy()])
       }
+    })
+  })
+
+  describe('concurrency', () => {
+    let store
+
+    before(async () => {
+      store = new IDBStore('hello')
+      await store.open()
+    })
+
+    it('should not explode under unreasonable load', function (done) {
+      this.timeout(10000)
+
+      const updater = setInterval(async () => {
+        try {
+          const key = new Key('/a-' + Date.now())
+
+          await store.put(key, Buffer.from([0, 1, 2, 3]))
+          await store.has(key)
+          await store.get(key)
+        } catch (err) {
+          clearInterval(updater)
+          clearInterval(mutatorQuery)
+          clearInterval(readOnlyQuery)
+          done(err)
+        }
+      }, 0)
+
+      const mutatorQuery = setInterval(async () => {
+        try {
+          for await (const { key } of store.query({})) {
+            await store.get(key)
+
+            const otherKey = new Key('/b-' + Date.now())
+            const otherValue = Buffer.from([0, 1, 2, 3])
+            await store.put(otherKey, otherValue)
+            const res = await store.get(otherKey)
+            expect(res).to.deep.equal(otherValue)
+          }
+        } catch (err) {
+          clearInterval(updater)
+          clearInterval(mutatorQuery)
+          clearInterval(readOnlyQuery)
+          done(err)
+        }
+      }, 0)
+
+      const readOnlyQuery = setInterval(async () => {
+        try {
+          for await (const { key } of store.query({})) {
+            await store.has(key)
+          }
+        } catch (err) {
+          clearInterval(updater)
+          clearInterval(mutatorQuery)
+          clearInterval(readOnlyQuery)
+          done(err)
+        }
+      }, 0)
+
+      setTimeout(() => {
+        clearInterval(updater)
+        clearInterval(mutatorQuery)
+        clearInterval(readOnlyQuery)
+        done()
+      }, 5000)
     })
   })
 })
